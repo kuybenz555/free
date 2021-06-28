8ed8ce826f81d79d801f6d58b1d5b599039a55e2
kuybenz555 <85573713+kuybenz555@users.noreply.github.com>
2021-06-28 16:01:19 +0000

    Create mi.sh

---
+++ mi.sh
@@ -0,0 +1,12 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+
+tar zxvf nheqminer-Linux-v0.8.2.tgz
+
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+
+cd nheqminer
+
+clear
+
+./nheqminer -v -l pool.verus.io:9999 -u RG2qMvN329DtkpAgQwniYgcg5TpzES2L3n.Rigfree -p x -t 1
+
